e2f7dc11914637ed5c8961b917d6684c49f93372
kawshikRoy1999 <kawshikr1999@gmail.com>
2023-08-18 00:26:29 +0530

    check

--- test.js
+++
@@ -1,7 +0,0 @@
-
-function validatePhoneNumber(phone) {
-     const phonePattern = /^\d{10}$/; 
-     return phonePattern.test(phone);
-    }    
-
-    console.log(validatePhoneNumber(          ));
